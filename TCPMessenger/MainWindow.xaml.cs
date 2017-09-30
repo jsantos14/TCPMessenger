a1e03579835217526e11c4ec1c5093b4368564cc
Joe Santos <jsantos14@georgefox.edu>
2017-09-30 14:40:26 -0700

    Add project files.

---
+++ TCPMessenger/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCPMessenger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            String oldConvo = convoBox.Text;
+            convoBox.Text = oldConvo + "\nYou: " + textBox.Text;
+            textBox.Text = ""; 
+        }
+
+        private void textBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                String oldConvo = convoBox.Text;
+                convoBox.Text = oldConvo + "\nYou: " + textBox.Text;
+                textBox.Text = ""; 
+                e.Handled = true;
+            }
+        }
+    }
+}
